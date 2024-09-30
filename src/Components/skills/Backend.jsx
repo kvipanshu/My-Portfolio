ad891642d2b3f58cfc30ea24cd17cd1d70f5b347
revising <vipanshu.srivastava28@gmail.com>
2024-09-30 15:55:26 +0530

    Added Skill

--- src/Components/skills/Backend.jsx
+++ src/Components/skills/Backend.jsx
@@ -54,6 +54,14 @@ const Backend = () => {
 							<span className="skills__level">Intermediate</span>
 						</div>
 					</div>
+
+					<div className="skills__data">
+						<i class="bx bx-badge-check"></i>
+						<div>
+							<h3 className="skills__name">WordPress</h3>
+							<span className="skills__level">Intermediate</span>
+						</div>
+					</div>
 				</div>
 			</div>
 		</div>
