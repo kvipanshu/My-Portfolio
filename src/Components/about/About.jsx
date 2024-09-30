af4166c9149d40f1d4b06401ef3deebad74a9e0c
revising <vipanshu.srivastava28@gmail.com>
2024-09-30 15:15:35 +0530

    CV changed

--- src/Components/about/About.jsx
+++ src/Components/about/About.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "./about.css";
 import AboutImg from "../../assets/Pic.jpg";
-import CV from "../../assets/VIPANSHU_CV.pdf";
+import CV from "../../assets/Vipanshu_CV.pdf";
 import Info from "./Info";
 
 const About = () => {
